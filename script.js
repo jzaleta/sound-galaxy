38c21024b619d9b4b74f1a2c77c426f5a703c98e
Javier Zaleta Martínez <94091554+j-cordz@users.noreply.github.com>
2023-04-23 00:12:35 -0600

    Update script.js particles movement

--- script.js
+++ script.js
@@ -4,16 +4,43 @@ let Particle = function (position) {
   this.position = position;
   this.speed = createVector(0, 1);
   this.color = [random(0, 255), random(0, 255), random(0, 255)];
+
   this.draw = function () {
     circle(this.position.x, this.position.y, this.diameter);
     fill(this.color);
   };
+
   this.update = function (energy) {
-    this.position.y += this.speed.y * energy * 10;
-    if (this.position.y > height) {
-      this.position.y = 0;
-    }
     this.diameter = random(5, 7) + energy * 100;
+
+    var centerV = height / 2;
+    var centerH = width / 2;
+
+    if (this.position.y < centerV) {
+      this.position.x -= this.speed.y * energy * 10;
+      this.position.y +=
+        ((this.position.x - centerH) / (this.position.y - centerV)) *
+        this.speed.y *
+        energy *
+        30;
+    } else {
+      this.position.x += this.speed.y * energy * 10;
+      this.position.y -=
+        ((this.position.x - centerH) / (this.position.y - centerV)) *
+        this.speed.y *
+        energy *
+        30;
+    }
+
+    if (
+      this.position.y < 0 ||
+      this.position.x < 0 ||
+      this.position.y > height ||
+      this.position.x > width
+    ) {
+      this.position.y = random(0, height);
+      this.position.x = random(0, width);
+    }
   };
 };
 
@@ -35,3 +62,4 @@ function draw() {
   let spectrum = fft.analyze();
   updateParticles(spectrum);
 }
+
