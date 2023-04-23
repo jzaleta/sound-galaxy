80c1254fd2261496ac3ad793af8aa370a15ab79f
Javier Zaleta Martínez <94091554+j-cordz@users.noreply.github.com>
2023-04-22 23:36:21 -0600

    Add index.html and setup P5.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let fft
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  noStroke()
+
+  let mic = new p5.AudioIn()
+  mic.start()
+
+  fft = new p5.FFT()
+  fft.setInput(mic)
+}
+
+function draw() {}
