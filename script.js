ca750df521b26d699c9c01dc260fe1506753db05
Javier Zaleta Martínez <94091554+j-cordz@users.noreply.github.com>
2023-04-23 00:01:53 -0600

    Apply JavaScript formatting

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-let fft
+let fft;
 
 let Particle = function (position) {
-  this.position = position
-  this.speed = createVector(0, 1)
-  this.color = [random(0, 255), random(0, 255), random(0, 255)]
+  this.position = position;
+  this.speed = createVector(0, 1);
+  this.color = [random(0, 255), random(0, 255), random(0, 255)];
   this.draw = function () {
-    circle(this.position.x, this.position.y, this.diameter)
-    fill(this.color)
-  }
+    circle(this.position.x, this.position.y, this.diameter);
+    fill(this.color);
+  };
   this.update = function (energy) {
-    this.position.y += this.speed.y * energy * 10
+    this.position.y += this.speed.y * energy * 10;
     if (this.position.y > height) {
-      this.position.y = 0
+      this.position.y = 0;
     }
-    this.diameter = random(5, 7) + energy * 100
-  }
-}
+    this.diameter = random(5, 7) + energy * 100;
+  };
+};
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  noStroke()
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
 
-  let mic = new p5.AudioIn()
-  mic.start()
+  let mic = new p5.AudioIn();
+  mic.start();
 
-  fft = new p5.FFT()
-  fft.setInput(mic)
+  fft = new p5.FFT();
+  fft.setInput(mic);
 
-  positionParticles()
+  positionParticles();
 }
 
 function draw() {
-  background(0, 0, 0)
-  let spectrum = fft.analyze()
-  updateParticles(spectrum)
+  background(0, 0, 0);
+  let spectrum = fft.analyze();
+  updateParticles(spectrum);
 }
